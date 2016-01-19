dc7d19a6268dec2965af8093cfbfeed7e649b202
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-19 08:52:32 -0800

    combined divisible by 400 with if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 function leapYear (year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)){
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
-  }
-  else if (year % 400 === 0) {
-    return true;
-  }
-  else {
+  } else {
   return false;
   }
 };
