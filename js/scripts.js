b6b9f2d5c83e8429555880af0549555592cdb2fe
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-19 08:47:58 -0800

    Add fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 function leapYear (year) {
-  if (year % 100 === 0) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  else if (year % 100 === 0) {
     return false;
   }
   else if (year % 4 === 0){
